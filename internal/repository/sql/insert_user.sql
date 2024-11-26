bf6473e352c3ab129bc9ec8bfd66812d22d2b5dc
giicoo <arnst.artyom@gmail.com>
2024-11-26 16:37:56 +0300

    repo refactor

--- internal/repository/sql/insert_user.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO users (email, hash_password) VALUES (?, ?);
--- internal/repository/sqlite/user_repo.go
+++ internal/repository/sqlite/user_repo.go
@@ -1,22 +1,16 @@
 package sqlite
 
 import (
-	"os"
-
 	"github.com/giicoo/GiicooAuth/internal/models"
 )
 
 func (sq *Sqlite) GetUserById(id int) (models.User, error) {
+	stmt := "SELECT * FROM users WHERE user_id = ?;"
 
-	file, err := os.ReadFile(sq.cfg.DB.PathToSQL + "get_user_by_id.sql")
-	if err != nil {
-		return models.User{}, err
-	}
 	user := models.User{}
 
-	stmt := string(file)
 	row := sq.db.QueryRow(stmt, id)
-	err = row.Scan(&user.UserId, &user.Email, &user.HashPassword)
+	err := row.Scan(&user.UserId, &user.Email, &user.HashPassword)
 	if err != nil {
 		return models.User{}, err
 	}
@@ -25,16 +19,12 @@ func (sq *Sqlite) GetUserById(id int) (models.User, error) {
 }
 
 func (sq *Sqlite) GetUserByEmail(email string) (models.User, error) {
+	stmt := "SELECT * FROM users WHERE email = ?;"
 
-	file, err := os.ReadFile(sq.cfg.DB.PathToSQL + "get_user_by_email.sql")
-	if err != nil {
-		return models.User{}, err
-	}
 	user := models.User{}
 
-	stmt := string(file)
 	row := sq.db.QueryRow(stmt, email)
-	err = row.Scan(&user.UserId, &user.Email, &user.HashPassword)
+	err := row.Scan(&user.UserId, &user.Email, &user.HashPassword)
 	if err != nil {
 		return models.User{}, err
 	}
@@ -43,12 +33,7 @@ func (sq *Sqlite) GetUserByEmail(email string) (models.User, error) {
 }
 
 func (sq *Sqlite) CreateUser(email string, hash_password string) error {
-
-	file, err := os.ReadFile(sq.cfg.DB.PathToSQL + "insert_user.sql")
-	if err != nil {
-		return err
-	}
-	stmt := string(file)
+	stmt := "INSERT INTO users (email, hash_password) VALUES (?, ?);"
 	s, err := sq.db.Exec(stmt, email, hash_password)
 	sq.log.Info(s)
 	if err != nil {
